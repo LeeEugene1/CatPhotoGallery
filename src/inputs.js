ecd2c2c2788723207419e2562d8b45c6fc8e6b53
eugene <uwm1004@gmail.com>
2021-11-26 20:38:32 +0900

    useRefBasic

--- src/inputs.js
+++ src/inputs.js
@@ -1,10 +1,11 @@
-import { useState } from "react/cjs/react.development";
+import { useState, useRef } from "react";
 
 function Inputs(){
     const [inputs, setInputs] = useState({
         name:'',
         nickname:''
     })
+    const nameInput = useRef()//DOM을 조작하기 위한 객체
     const onChangeName = (e) =>{
         console.log(e.target.value)
         //기존객체복사
@@ -29,11 +30,12 @@ function Inputs(){
             name:'',
             nickname:''
         })
+        nameInput.current.focus()
     }
     return(
         <div>
-            <input name="name" placeholder="이름" onChange={onChangeName}></input>
-            <input nickname="nickname" placeholder="닉네임" onChange={onChangeNickname}></input>
+            <input name="name" placeholder="이름" onChange={onChangeName} value={inputs.name} ref={nameInput}></input>
+            <input nickname="nickname" placeholder="닉네임" onChange={onChangeNickname} value={inputs.nickname}></input>
             <button onClick={onReset}>초기화</button>
             <b>값: {inputs.name}({inputs.nickname})</b>
         </div>
