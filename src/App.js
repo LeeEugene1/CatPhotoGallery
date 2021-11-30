c09d89eff19a10390ab17fd9f5cd2ea32770f2a1
eugene <uwm1004@gmail.com>
2021-12-01 00:06:20 +0900

    delete reacttutorials

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 // import './App.css';
 import React from 'react';
-import Home from './Home';
-import About from './About';
-import {Route,Routes,Link} from 'react-router-dom';
-
 function App() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-      </Routes>
-    </div>
-    
+    <></>
   );
 }
 
